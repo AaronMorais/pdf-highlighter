9cd6d2052d5ba61c511f609a2cfd073d316df1cd
Aaron Morais <aaron@candle.health>
2021-04-24 12:58:10 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: "pdf-highlighter",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-ZXE9S8EMM4",
+      },
+    },
+  ],
+};
