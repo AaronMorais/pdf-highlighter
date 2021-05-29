36cf8c9f4cef7de5c623df5c72381c43b2537d07
Aaron Morais <aaron@candle.health>
2021-05-28 21:46:30 -0400

    PDF highlight implementation with highlighting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,182 +1,268 @@
 import * as React from "react"
+import {useState} from "react"
+
+import Button from 'react-bootstrap/Button'
+import Spinner from 'react-bootstrap/Spinner'
+import * as pdfjsLib from 'pdfjs-dist'
+import { PDFDocument, Page, rgb, degrees, grayscale } from 'pdf-lib'
+import download from 'downloadjs'
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.js`;
 
 // styles
 const pageStyles = {
   color: "#232129",
-  padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  height: '100vh',
 }
 const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+  marginTop: 12,
+  marginBottom: 36,
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'center',
+  alignItems: 'center',
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
+const mainContainerStyles = {
+  display: 'flex',
+  width: '100%',
+  height: '100%',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+const optionMenuStyles = {
+  display: 'flex',
+  maxWidth: '400px',
+  height: '100%',
+  flexDirection: 'column',
+  marginLeft: 8,
+  marginRight: 32,
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
+const menuItemStyles = {
   marginBottom: 24,
+  fontWeight: 'bold',
 }
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
+const menuItemHeaderStyles = {
+  fontWeight: 'bold',
+  marginBottom: 6,
 }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
+const menuItemContextStyles = {
+  fontWeight: 'bold',
+  marginLeft: 6,
+  marginBottom: 6,
 }
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
+const phraseInputStyles = {
+  width: '100%',
 }
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const phraseContextInputStyles = {
+  maxWidth: 60,
+}
 
-// markup
+const updateButtonStyles = {
+  marginTop: 24,
+  height: 40,
+}
+
+const pagesToRemove = (numPages, pages, contextBefore, contextAfter) => {
+  let pagesSet = new Set(pages);
+  for (let i = 0; i < pages.length; i++) {
+    const page = pages[i];
+    for (let j = (-1 * contextBefore); j < contextAfter + 1; j++) {
+      const contextPage = page + j;
+      pagesSet.add(contextPage);
+    }
+  }
+
+  var result = [];
+  for (var i = numPages - 1; i >= 0; i--) {
+    if (!(pagesSet.has(i))) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
 const IndexPage = () => {
+  const [file, setFile] = useState(null);
+  const [phrases, setPhrases] = useState([]);
+  const [shouldHighlightPhrases, setShouldHighlightPhrases] = useState(true);
+  const [onlyShowPhrasePages, setOnlyShowPhrasePages] = useState(true);
+  const [phrasePageContextBefore, setPhrasePageContextBefore] = useState(1);
+  const [phrasePageContextAfter, setPhrasePageContextAfter] = useState(1);
+  const [processing, setProcessing] = useState(false);
+  const [result, setResult] = useState(null);
+
+  const onFileSelection = (event) => {
+    setFile(event.target.files[0]);
+  }
+
+  const onPhrasesChange = (event) => {
+    setPhrases(event.target.value.split(',').map((phrase) => phrase.trim().toLowerCase()))
+  }
+
+  const onShouldHighlightPhrasesChange = (event) => {
+    setShouldHighlightPhrases(event.target.checked)
+  }
+
+  const onShouldOnlyShowPhrasePagesChange = (event) => {
+    setOnlyShowPhrasePages(event.target.checked)
+  }
+
+  const onPhrasePageContextBeforeChange = (event) => {
+    setPhrasePageContextBefore(event.target.value)
+  }
+
+  const onPhrasePageContextAfterChange = (event) => {
+    setPhrasePageContextAfter(event.target.value)
+  }
+
+  const onSubmit = () => {
+    console.log(`Processing started`);
+    const start = Date.now();
+    setProcessing(true)
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(file);
+    fileReader.onload = function() {
+      let document = pdfjsLib.getDocument({data: fileReader.result});
+      document.promise.then((pdf) => {
+        let pageFetchPromises = []
+        const numPages = pdf.numPages;
+        for (let i = 1; i <= numPages; i++) {
+          pageFetchPromises.push(pdf.getPage(i));
+        }
+        
+        Promise.all(pageFetchPromises).then((pages) => {
+          return Promise.all(pages.map((page) => page.getTextContent()))
+        }).then((pageTextContent) => {
+          let pagesWithContent = []
+          let elementsToHighlight = {}
+          for (let i = 0; i < pageTextContent.length; i++) {
+            const content = pageTextContent[i];
+            if (!('items' in content)) {
+              continue;
+            }
+            if (onlyShowPhrasePages) {
+              const text = pageTextContent[i]['items'].map((c) => c.str).join('').toLowerCase();
+              for (let j = 0; j < phrases.length; j++) {
+                if (phrases[j].length > 0 && text.includes(phrases[j])) {
+                  pagesWithContent.push(i)
+                  break;
+                }
+              }
+            }
+            if (shouldHighlightPhrases) {
+              pageTextContent[i]['items'].map((item) => {
+                for (let j = 0; j < phrases.length; j++) {
+                  if (phrases[j].length > 0 && item.str.toLowerCase().includes(phrases[j].toLowerCase())) {
+                    if (!(i in elementsToHighlight)) {
+                      elementsToHighlight[i] = [];
+                    }
+                    elementsToHighlight[i].push([phrases[j], item]);
+                  }
+                }
+              });
+            }
+          }
+
+          PDFDocument.load(fileReader.result).then((pdfLoadedDocument) => {
+            if (onlyShowPhrasePages) {
+              const toRemove = pagesToRemove(numPages, pagesWithContent, phrasePageContextBefore, phrasePageContextAfter);
+              for (let i = 0; i < toRemove.length; i++) {
+                pdfLoadedDocument.removePage(toRemove[i]);
+              }
+            }
+            console.log(elementsToHighlight);
+            if (shouldHighlightPhrases) {
+              const keys = Object.keys(elementsToHighlight);
+              for (let i = 0; i < keys.length; i++) {
+                const pageNumber = parseInt(keys[i]);
+                const page = pdfLoadedDocument.getPage(parseInt(keys[i]))
+                for (let j = 0; j < elementsToHighlight[pageNumber].length; j++) {
+                  const phrase = elementsToHighlight[pageNumber][j][0]
+                  const transform = elementsToHighlight[pageNumber][j][1].transform
+                  const height = elementsToHighlight[pageNumber][j][1].height
+
+                  const elementLength = elementsToHighlight[pageNumber][j][1].str.length
+                  const elementUnit = elementsToHighlight[pageNumber][j][1].width / elementLength
+                  const phraseStartIndex = elementsToHighlight[pageNumber][j][1].str.toLowerCase().search(phrase.toLowerCase())
+                  const phraseEndIndex = phrase.length + phraseStartIndex
+
+                  const leftOffset = phraseStartIndex * elementUnit
+                  const rightOffset = (elementLength - phraseEndIndex) * elementUnit
+                  const width = elementsToHighlight[pageNumber][j][1].width - leftOffset - rightOffset
+                  
+                  page.drawRectangle({
+                    x: transform[4] + leftOffset,
+                    y: transform[5],
+                    height: height,
+                    width: width,
+                    color: rgb(1, 1, 0),
+                    opacity: 0.5,
+                  })
+                }
+              }
+            }
+            pdfLoadedDocument.save().then((pdfResult) => {
+              download(pdfResult, 'processed_' + file.name, "application/pdf");
+              setProcessing(false);
+              console.log(`Processing complete!`);
+              console.log(`Processing took ${Math.floor((Date.now() - start) / 1000)} seconds`);
+            })
+          });
+        })
+      })
+    };
+  }
+
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <title>PDF Highlight</title>
+      <h1 style={headingStyles}><span>PDF Highlight</span></h1>
+      <div style={mainContainerStyles}>
+        <div style={optionMenuStyles}>
+          <div style={menuItemStyles}>
+            <div style={menuItemHeaderStyles}>Select PDF</div>
+            <input type="file" onChange={onFileSelection} />
+          </div>
+          <div style={menuItemStyles}>
+            <div style={menuItemHeaderStyles}>Phrases</div>
+            <textarea onChange={onPhrasesChange} style={phraseInputStyles} placeholder={"Comma separated phrases\nie. 'source water, geological, landfill'"}/>
+          </div>
+          <div style={menuItemStyles}>
+            <input type="checkbox" defaultChecked={true} onChange={onShouldHighlightPhrasesChange} />
+            <span style={menuItemContextStyles}>Highlight phrases</span>
+          </div>
+          <div>
+            <input type="checkbox" defaultChecked={true} onChange={onShouldOnlyShowPhrasePagesChange} />
+            <span style={menuItemContextStyles}>Only include pages with phrases</span>
+          </div>
+          {onlyShowPhrasePages && 
+            <>
+              <div>
+                <input style={phraseContextInputStyles} type="number" min={0} value={phrasePageContextBefore} onChange={onPhrasePageContextBeforeChange} />
+                <span style={menuItemContextStyles}>page(s) before</span>
+              </div>
+              <div>
+                <input style={phraseContextInputStyles} type="number" min={0} value={phrasePageContextAfter} onChange={onPhrasePageContextAfterChange} />
+                <span style={menuItemContextStyles}>page(s) after</span>
+              </div>
+            </>
+          }
+          <Button style={updateButtonStyles} variant="primary" disabled={processing || file === null} onClick={onSubmit}>
+            {processing ? 'Loading...' : 'Submit'} 
+          </Button>
+        </div>
+      </div>
     </main>
   )
 }
